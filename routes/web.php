52a14308189cc0e6d06be6eece43fdc5760f1919
roland-pixel <kharis7181@gmail.com>
2025-10-17 21:39:15 +0700

    updat 7

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BukuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,4 +11,9 @@ Route::get('/login', function () {
     return view('login');
 });
 
-Route::resource('bukus', App\Http\Controllers\BukuController::class);
+Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
+Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
+Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
+Route::get('/buku/{id}/edit', [BukuController::class, 'edit'])->name('buku.edit');
+Route::put('/buku/{id}', [BukuController::class, 'update'])->name('buku.update');
+Route::delete('/buku/{id}', [BukuController::class, 'destroy'])->name('buku.destroy');

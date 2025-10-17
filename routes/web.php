143405ae8c961dbd91b1599083ff1ff159cc7e4b
roland-pixel <kharis7181@gmail.com>
2025-10-17 21:43:33 +0700

    update8

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ Route::get('/login', function () {
 Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
 Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
 Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
-Route::get('/buku/{id}/edit', [BukuController::class, 'edit'])->name('buku.edit');
-Route::put('/buku/{id}', [BukuController::class, 'update'])->name('buku.update');
-Route::delete('/buku/{id}', [BukuController::class, 'destroy'])->name('buku.destroy');
+Route::get('/buku/{buku}/edit', [BukuController::class, 'edit'])->name('buku.edit');
+Route::put('/buku/{buku}', [BukuController::class, 'update'])->name('buku.update');
+Route::delete('/buku/{buku}', [BukuController::class, 'destroy'])->name('buku.destroy');
